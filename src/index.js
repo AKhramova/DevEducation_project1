8d8f901f1443253989cef6d35bfa5646d4ef751f
Anastasiia <khramovanastasiia@gmail.com>
2021-04-09 15:06:58 +0300

    page questions filtration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './styles/styles.scss'
 import filter from './modules/filter';
 import homePageModule from './modules/home-page-request';
 import questionsPageModule from './modules/questions';
+import { render } from './modules/filter'
 
 window.addEventListener('DOMContentLoaded', () => {
   // Тут вызываем все модули
   if (window.location.pathname === '/questions.html') {
     questionsPageModule();
     filter();
+    render();
   } else if (window.location.pathname === '/about.html') {
     // страница about; 
   } else if (window.location.pathname === '/index.html') {
